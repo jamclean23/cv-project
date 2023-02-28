30d19da3b70519f683db4fa805842645ae26c6fa
jamclean23 <jamclean23@gmail.com>
2023-02-28 13:29:15 -0500

    Add React App integration

---
+++ src/index/App.js
@@ -0,0 +1,16 @@
+// Main React App
+
+// ====== IMPORTS ======
+import React from 'react';
+
+// ====== Components ======
+
+function App () {
+    return (
+        <h1>Hello world</h1>
+    );
+}
+
+export {
+    App
+}
--- src/index/index.js
+++ src/index/index.js
@@ -1,3 +1,16 @@
-// Entry point
-import css from './styles.css';
+// Entry for React
 
+//  ====== IMPORTS =======
+
+import './styles.css';
+import React from 'react';
+import ReactDom from 'react-dom/client';
+import { App } from './App.js';
+
+// ====== ROOT CREATION ======
+
+const root = ReactDom.createRoot(document.querySelector('#root'));
+
+// ====== RENDER ======
+
+root.render(<App />);
